fcd8def885a4bb7ea754ec5033ccf0b13e52284a
Jingtian Huang <huang.jin@husky.neu.edu>
2018-05-16 17:17:06 -0700

    Add update & delete user following the lecture

--- src/main/java/com/example/webapp/services/UserService.java
+++ src/main/java/com/example/webapp/services/UserService.java
@@ -1,10 +1,14 @@
 package com.example.webapp.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,11 @@ public class UserService {
 	@Autowired
 	UserRepository repository;
 	
+	@DeleteMapping("/api/user/{userId}")
+	public void deleteUser(@PathVariable("userId") int id) {
+		repository.deleteById(id);
+	}
+	
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return (List<User>) repository.findAll();
@@ -24,5 +33,26 @@ public class UserService {
 	@PostMapping("/api/user")
 	public User createUser(@RequestBody User user) {
 		return repository.save(user);
+	}
+	
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") int id) {
+		Optional<User> data = repository.findById(id);
+		if(data.isPresent()) {
+			return data.get();
+		}
+		return null;
+	}
+	
+	@PutMapping("/api/user/{userId}")
+	public User updateUser(@PathVariable("userId") int userId, @RequestBody User newUser) {
+		Optional<User> data = repository.findById(userId);
+		if(data.isPresent()) {
+			User user = data.get();
+			user.setFirstName(newUser.getFirstName());
+			repository.save(user);
+			return user;
+		}
+		return null;
 	}
 }
---
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -0,0 +1,70 @@
+function UserServiceClient() {
+    this.createUser = createUser;
+    this.findAllUsers = findAllUsers;
+    this.findUserById = findUserById;
+    this.deleteUser = deleteUser;
+    this.updateUser = updateUser;
+    this.login = login;
+    this.url =
+        'http://localhost:8080/api/user';
+    var self = this;
+
+    function login(username, password) {
+        return fetch(self.url, {
+            method:'post',
+            body: JSON.stringify({username:username,password:password}),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+
+    function createUser(user) {
+         return fetch(self.url, {
+            method:'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+    }
+
+    function updateUser(userId,user) {
+        return fetch(self.url + '/' + userId, {
+            method: 'put',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(function(response) {
+            if(response.bodyUsed) {
+                return response.json();
+            } else {
+                return null;
+            }
+        });
+    }
+
+
+    function findAllUsers() {
+        return fetch(self.url)
+            .then(function (response) {
+            return response.json();
+        });
+    }
+
+    function deleteUser(userId) {
+        return fetch(self.url + '/' + userId, {
+            method:'delete'
+        });
+    }
+
+    function findUserById(userId) {
+        return fetch(self.url + '/' + userId)
+            .then(function(response) {
+                return response.json();
+            });
+    }
+
+}
