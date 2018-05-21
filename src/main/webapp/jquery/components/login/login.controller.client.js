7e8abe8b081ca1c6b644bef40b88567a44d74b7b
Jingtian Huang <huang.jin@husky.neu.edu>
2018-05-20 23:34:06 -0700

    Implement login function in login controller and service on client side

--- src/main/webapp/jquery/components/login/login.controller.client.js
+++ src/main/webapp/jquery/components/login/login.controller.client.js
@@ -11,12 +11,27 @@
     }
 
     function login() {
-        var username = $usernameFld.val();
-        var password = $passwordFld.val();
-        if(!username && !password) {
-            alert("Please input the username and password.");
+        var usernameVal = $usernameFld.val();
+        var passwordVal = $passwordFld.val();
+        if(!usernameVal) {
+            alert("The username can't be empty.");
+        } else if(!passwordVal) {
+            alert("The password can't be empty");
         } else {
-            userService.login(username,password);
+            var user = new User();
+            user.setUsername(usernameVal);
+            user.setPassword(passwordVal);
+            console.log(user);
+            userService.login(user)
+                .then(function(response) {
+                    if(response.status === 200) {
+                        alert("Success!");
+                    } else if(response.status === 500) {
+                        alert("No matching user found.");
+                    } else {
+                        alert("Something is wrong, but try again later");
+                    }
+                });
         }
     }
 })();
