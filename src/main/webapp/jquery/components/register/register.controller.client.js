5626101dcd680f0aa8469e5b983d3d60eccbed9d
Jingtian Huang <huang.jin@husky.neu.edu>
2018-05-21 22:38:24 -0700

    fix the navigation url for sign up

--- src/main/webapp/jquery/components/register/register.controller.client.js
+++ src/main/webapp/jquery/components/register/register.controller.client.js
@@ -28,7 +28,7 @@
                 .then(function(response) {
                     if(response.status === 200) {
                         window.location.href =
-                            'http://localhost:8080/jquery/components/profile' +
+                            '/jquery/components/profile' +
                             '/profile.template.client.html?username=' + usernameVal;
                     } else if(response.status === 500) {
                         alert("Username already exist.");
