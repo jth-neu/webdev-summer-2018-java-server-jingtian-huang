811b4aa48c13627faa90c499677fc0f08080638b
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-01 10:53:04 -0700

    Add Lesson data model

--- src/main/java/com/example/webapp/models/Module.java
+++ src/main/java/com/example/webapp/models/Module.java
@@ -1,10 +1,13 @@
 package com.example.webapp.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
@@ -16,6 +19,9 @@ public class Module {
 	@ManyToOne
 	@JsonIgnore
 	private Course course;
+	@OneToMany(mappedBy="module")
+	private List<Lesson> lessons;
+	
 	public int getId() {
 		return id;
 	}
@@ -34,4 +40,11 @@ public class Module {
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+	public List<Lesson> getLessons() {
+		return lessons;
+	}
+	public void setLessons(List<Lesson> lessons) {
+		this.lessons = lessons;
+	}
+	
 }
