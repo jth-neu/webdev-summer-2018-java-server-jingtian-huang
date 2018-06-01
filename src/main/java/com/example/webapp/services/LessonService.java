411f53a9f5a9e143ef683fab0e609414f0481ea2
Jingtian Huang <huang.jin@husky.neu.edu>
2018-06-01 13:19:35 -0700

    Add findLessonsForModule

--- src/main/java/com/example/webapp/services/LessonService.java
+++ src/main/java/com/example/webapp/services/LessonService.java
@@ -1,14 +1,17 @@
 package com.example.webapp.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.webapp.models.Course;
 import com.example.webapp.models.Lesson;
 import com.example.webapp.models.Module;
 import com.example.webapp.repositories.LessonRepository;
@@ -35,6 +38,18 @@ public class LessonService {
 			Module module = data.get();
 			newLesson.setModule(module);
 			return lessonRepository.save(newLesson);
+		}
+		return null;		
+	}
+	
+	@GetMapping("/api/course/{courseId}/module/{moduleId}/lesson")
+	public List<Lesson> findAllLessonsForModule(
+			@PathVariable("courseId") int courseId,
+			@PathVariable("moduleId") int moduleId) {
+		Optional<Module> data = moduleRepository.findById(moduleId);
+		if(data.isPresent()) {
+			Module module = data.get();
+			return module.getLessons();
 		}
 		return null;		
 	}
